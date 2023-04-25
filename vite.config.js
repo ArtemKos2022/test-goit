65aaea7f2cb38402d5afa07c082a3bb64218db38
ArtemKos2022 <110256138+ArtemKos2022@users.noreply.github.com>
2023-04-25 22:47:42 +0300

    add fille

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+    base: "/test-goit/",
+    plugins: [react()],
+    server: {
+        open: './',
+        port: 8888,
+    },
+    preview: {
+        open: './',
+        port: 8080,
+    },
+})
